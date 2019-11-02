09bb57560949aecd83c6c4c6d0a0d200949513d0
salkhafaji <sajjad.jawad.alkhafaji@mail.ru>
2019-11-02 15:59:03 +0300

    add git commands

---
+++ git/commands.sh
@@ -0,0 +1,16 @@
+
+#undo last push
+git push -f origin HEAD^:master
+
+#tags
+git tag
+
+#push tag
+git push origin #tag name here#
+
+#push all tags
+git push origin --tags
+
+#delete local tag
+git tag -d #tag name here#
+
