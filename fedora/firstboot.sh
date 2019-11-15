be2b736c7222689d334a84c5aef372d8f6819d48
Sajjad <sajjad.jawad.alkhafaji@mail.ru>
2019-11-15 16:33:08 +0300

    first boot command

---
+++ fedora/firstboot.sh
@@ -0,0 +1,16 @@
+
+# imp Packages
+sudo dnf install make
+sudo dnf install zlib-devel
+
+# Vim
+sudo dnf install vim
+
+# Snap
+sudo dnf install snapd
+
+# install google chrome
+sudo dnf install fedora-workstation-repositories
+sudo dnf config-manager --set-enabled google-chrome
+sudo dnf install google-chrome-stable
+
