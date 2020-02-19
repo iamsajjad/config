dc129bd054658e55fc5f6d4cf5f2e19ce352051f
Sajjad <iamsajjad@mail.ru>
2020-02-19 16:35:40 +0300

    add telegram

---
+++ fedora/apps.sh
@@ -0,0 +1,9 @@
+
+# Telegram Desktop
+sudo dnf copr enable rommon/telegram
+sudo dnf install telegram-desktop
+
+# commond-line Telegram
+sudo dnf copr enable iranzo/telegram-cli
+sudo dnf install telegram-cli
+
