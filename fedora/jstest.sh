c764e7ba56db15c613db5e198223b0815252095d
iamsajjad <iamsajjad@mail.ru>
2020-03-21 03:53:47 +0430

    fix xbox one controller

--- fedora/jstest.sh
+++ fedora/jstest.sh
@@ -1,2 +1,9 @@
 sudo dnf install kernel-modules-extra
 sudo dnf install joystick
+
+# Xbox one Controller
+sudo dnf install sysfsutils
+sudo vim /etc/sysfs.conf
+# add '/module/bluetooth/parameters/disable_ertm=1' at the end
+#jstest /dev/input/js0
+
