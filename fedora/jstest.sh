edaae790f516f587cb329e07820724d5ed582eb5
iamsajjad <iamsajjad@mail.ru>
2020-03-21 04:52:22 +0430

    fix xbox one controller disconnected

--- fedora/jstest.sh
+++ fedora/jstest.sh
@@ -1,15 +1,16 @@
 sudo dnf install kernel-modules-extra
 sudo dnf install joystick
+#jstest /dev/input/js0
 
 # Xbox one Controller
 sudo dnf install sysfsutils
-sudo vim /etc/sysfs.conf
-# add '/module/bluetooth/parameters/disable_ertm=1' at the end
-#jstest /dev/input/js0
+# replace N to 1 in this file'/module/bluetooth/parameters/disable_ertm'
+sudo echo 1 > /sys/module/bluetooth/parameters/disable_ertm
 
 #play around bluetooth
-sudo echo 1 > /sys/module/bluetooth/parameters/disable_ertm
 bluetoothctl
 scan on
+remove xxx (the Mac of the controller)
 pair xxx (the Mac of the controller)
+trust xxx (the Mac of the controller)
 connect xxx (the mac of the controller)
