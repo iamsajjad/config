b2370dc9b087e0d8ae9c98c9173a9e377af67f62
iamsajjad <iamsajjad@mail.ru>
2020-03-21 04:31:28 +0430

    add bluetoothctl

--- fedora/jstest.sh
+++ fedora/jstest.sh
@@ -7,3 +7,9 @@ sudo vim /etc/sysfs.conf
 # add '/module/bluetooth/parameters/disable_ertm=1' at the end
 #jstest /dev/input/js0
 
+#play around bluetooth
+sudo echo 1 > /sys/module/bluetooth/parameters/disable_ertm
+bluetoothctl
+scan on
+pair xxx (the Mac of the controller)
+connect xxx (the mac of the controller)
