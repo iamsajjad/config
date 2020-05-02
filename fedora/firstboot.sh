60ec9f926829fad2dba01ae67d7da0904adff849
iamsajjad <iamsajjad@mail.ru>
2020-05-02 23:28:26 +0300

    add -y option for package to install

--- fedora/firstboot.sh
+++ fedora/firstboot.sh
@@ -1,18 +1,19 @@
 
-# imp Packages
-sudo dnf install make
-sudo dnf install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel libevent-devel sqlite-devel
+# Theme, font ...
+sudo dnf -y install gnome-tweak-tool
 
 # Vim
-sudo dnf install vim
+sudo dnf -y install vim
 
-# Snap
-sudo dnf install snapd
+# To Create Bootable USB Stick Using ISO File
+sudo dnf -y install mediawriter
+
+# Packages
+sudo dnf -y install make
+sudo dnf -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel libevent-devel sqlite-devel
 
 # install google chrome
-sudo dnf install fedora-workstation-repositories
-sudo dnf config-manager --set-enabled google-chrome
-sudo dnf install google-chrome-stable
+sudo dnf -y install fedora-workstation-repositories
+sudo dnf -y config-manager --set-enabled google-chrome
+sudo dnf -y install google-chrome-stable
 
-# Theme, font ...
-sudo dnf install gnome-tweak-tool
--- fedora/mediawriter.sh
+++ fedora/mediawriter.sh
@@ -1,6 +1,6 @@
 
 # To Create Bootable USB Stick Using ISO File
-sudo dnf install mediawriter
+sudo dnf -y install mediawriter
 
 # Or Use dd like
 #sudo dd if=Fedora-Workstation-Live-x86_64-30-1.2.iso of=/dev/sdc
