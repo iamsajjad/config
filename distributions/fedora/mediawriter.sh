084671a0cb9cf68e71e8cd380fd5b81b14548187
iamsajjad <iamsajjad@mail.ru>
2020-05-22 06:27:59 +0300

    add some doc for dd

--- distributions/fedora/mediawriter.sh
+++ distributions/fedora/mediawriter.sh
@@ -2,5 +2,8 @@
 # To Create Bootable USB Stick Using ISO File
 sudo dnf -y install mediawriter
 
-# Or Use dd like
-#sudo dd if=Fedora-Workstation-Live-x86_64-30-1.2.iso of=/dev/sdc
+# Or Use dd it work for windows too
+#sudo dd bs=4M if=[ur .iso] of=/dev/sd[that 1 letter]
+#sudo dd bs=4M if=Fedora-Workstation-Live-x86_64-30-1.2.iso of=/dev/sdc
+
+
