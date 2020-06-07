4dfff6afe6e97547d69d300920791e5809bc057e
iamsajjad <iamsajjad@mail.ru>
2020-06-07 16:15:15 +0300

    create vim setup to setup vim configurations

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,1 +1,15 @@
+# setup.sh
+
+# copy configs files to home
+cp .vimrc ~/
+cp -R vim/ ~/.config/
+cp -R dotvim/. ~/.vim/
+
+# install vim plug plugins manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install plugins
+vim +PlugInstall +qall
+
+echo "VIM configs done."
 
